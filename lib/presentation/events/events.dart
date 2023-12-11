e8996df92700bfe9737206f8f79aa6f3b61185c6
JesbinJose <jesbinjose57@gmail.com>
2023-12-11 10:34:35 +0530

    event ui updated

--- lib/presentation/events/events.dart
+++ lib/presentation/events/events.dart
@@ -1,3 +1,5 @@
+import 'package:cyberbee_web/constants.dart';
+import 'package:cyberbee_web/core/firebase/function/events/events.dart';
 import 'package:flutter/material.dart';
 
 class EventScreen extends StatelessWidget {
@@ -6,7 +8,49 @@ class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      // children: [SizedBox(width: double.infinity,height: 200,child: StreamBuilder(stream: stream, builder: builder),)],
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 200,
+          child: StreamBuilder(
+            stream: MyFirebaseEvents.getAllEvents(),
+            builder: (context, snapshot) {
+              if (snapshot.data == null) return const SizedBox();
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final event = snapshot.data!.docs[index];
+                  return Container(
+                    decoration: const BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              10,
+                            ),
+                          ),
+                          child: Image.network(
+                            event['image'],
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
